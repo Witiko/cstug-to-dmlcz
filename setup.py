467f4010e26a70d7f240eb8809eeb15720bca85e
Vít Novotný <witiko@mail.muni.cz>
2021-08-18 18:11:56 +0200

    Support Python 3.8 and 3.9

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Text Processing :: Markup :: XML',
         'Topic :: Utilities',
     ],
