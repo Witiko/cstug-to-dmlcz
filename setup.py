f4f0d5d1c6d3d31423dcacd29be57cac5253ea6c
Vít Starý Novotný <witiko@mail.muni.cz>
2025-12-10 16:12:49 +0100

    Update CI

--- setup.py
+++ setup.py
@@ -39,8 +39,6 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
         'Topic :: Text Processing :: Markup :: XML',
         'Topic :: Utilities',
     ],
